cd5c9ebf010f8dc196d2e825b74349acc94e42c3
Soaurabh Kakkar <soaurabh.kakkar@gmail.com>
2019-07-20 18:05:55 +0530

    Changes to fix rotation for SlideInViewLauncher

--- Part-IV/HLSPlayer/HLSPlayer/SlideInViewLauncher.swift
+++ Part-IV/HLSPlayer/HLSPlayer/SlideInViewLauncher.swift
@@ -11,7 +11,7 @@ import UIKit
 class SlideInViewLauncher {
     private let slideInView: UIView
     private var isViewVisible = false
-    
+    private var slideInViewBottomConstraint: NSLayoutConstraint?
     private lazy var blurView: UIView = {
         let blurView = UIView()
         blurView.backgroundColor = UIColor(white: 0, alpha: 0.55)
@@ -45,16 +45,28 @@ class SlideInViewLauncher {
         window.addSubview(blurView)
         window.addSubview(slideInView)
         
-        let originY = window.frame.height - height
-        slideInView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
-        blurView.frame = window.frame
-        blurView.alpha = 0
+        slideInView.translatesAutoresizingMaskIntoConstraints = false
+        slideInViewBottomConstraint = slideInView.bottomAnchor.constraint(equalTo:  window.bottomAnchor, constant: height)
+        NSLayoutConstraint.activate([slideInView.leadingAnchor.constraint(equalTo:  window.leadingAnchor),
+                                     slideInView.trailingAnchor.constraint(equalTo:  window.trailingAnchor),
+                                     slideInView.heightAnchor.constraint(equalToConstant: height),
+                                     slideInView.widthAnchor.constraint(equalTo: window.widthAnchor),
+                                     slideInViewBottomConstraint!])
         
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([blurView.leadingAnchor.constraint(equalTo:  window.leadingAnchor),
+                                     blurView.trailingAnchor.constraint(equalTo:  window.trailingAnchor),
+                                     blurView.topAnchor.constraint(equalTo:  window.topAnchor),
+                                     blurView.bottomAnchor.constraint(equalTo:  window.bottomAnchor)])
+        blurView.alpha = 0
+
+        window.layoutIfNeeded()
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: { [weak self] in
             guard let self = self else { return }
             self.blurView.alpha = 1
-            self.slideInView.frame = CGRect(x: 0, y: originY, width: self.slideInView.frame.width, height: self.height)
-            }, completion: nil)
+            self.slideInViewBottomConstraint?.constant = 0
+            window.layoutIfNeeded()
+        }, completion: nil)
         
         isViewVisible.toggle()
     }
@@ -63,11 +75,16 @@ class SlideInViewLauncher {
     @objc func dismiss() {
         // Return if view is already dismissed or key-window is not available
         guard isViewVisible, let window = self.window else { return }
-        UIView.animate(withDuration: 0.5) { [weak self] in
+        
+        UIView.animate(withDuration: 0.5, animations: { [weak self] in
             guard let self = self else { return }
             self.blurView.alpha = 0
-            self.slideInView.frame = CGRect(x: 0, y: window.frame.height, width: self.slideInView.frame.width, height: self.height)
-        }
+            self.slideInViewBottomConstraint?.constant = self.height
+            window.layoutIfNeeded()
+            }, completion: { _ in
+                self.blurView.removeFromSuperview()
+                self.slideInView.removeFromSuperview()
+        })
         isViewVisible.toggle()
     }
     
@@ -77,5 +94,3 @@ class SlideInViewLauncher {
         slideInView.removeFromSuperview()
     }
 }
-
-
